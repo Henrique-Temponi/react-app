e77493bc1513eb7c702c7cf32dcf1410388c803a
Henrique-temponi <tanzenheavely@gmail.com>
2021-06-23 20:27:20 -0300

    Add new components

---
+++ src/Table.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Mac</td>
+                <td>Bouncer</td>
+            </tr>
+            <tr>
+                <td>Dee</td>
+                <td>Aspiiring actress</td>
+            </tr>
+            <tr>
+                <td>Dennis</td>
+                <td>Bartender</td>
+            </tr>
+        </tbody>
+    );
+}
+
+class Table extends Component {
+    render() {
+        return (
+            <table>
+                <TableHeader />
+                <TableBody />
+            </table>
+        )
+    }
+}
+
+export default Table;
